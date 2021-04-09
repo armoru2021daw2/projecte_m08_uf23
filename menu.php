8ea20cd943ebe753ac3b2542a45becde29b23932
armoru <armoru@zend-armoru.fjeclot.net>
2021-04-09 19:38:01 +0200

    projecte

---
+++ menu.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>
+			MENÚ PRINCIPAL 
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS</h2>
+		<h3> <b>Fet per: Aran Morell</b> </h3>
+		<a href="http://zend-armoru.fjeclot.net/armoru2021daw2/visualitzar.php">Visualitzar usuaris</a><br>
+		<a href="http://zend-armoru.fjeclot.net/armoru2021daw2/afegir.php">Afegir usuaris</a><br>
+		<a href="http://zend-armoru.fjeclot.net/armoru2021daw2/modificar.php">Modificar usuaris</a><br>
+		<a href="http://zend-armoru.fjeclot.net/armoru2021daw2/esborrar.php">Esborrar usuaris</a><br>
+		
+	</body>
+</html>
